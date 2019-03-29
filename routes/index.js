2d55d69e4d57a4007c0cb6da12353bdfd1d113db
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2019-03-30 02:45:17 +0530

    Added support for cummulative file

--- routes/index.js
+++ routes/index.js
@@ -12,31 +12,21 @@ router.get('/', function(req, res, next) {
 router.get("/plot", function(req, res, next){
     res.render("plot");
 })
+
 router.post("/view", function(req, res, next){
     var form = new multiparty.Form();
     form.parse(req, function(err, fields, files) {
-        tempPath = files.temp[0]["path"];
-        radiPath = files.radi[0]["path"];
-        console.log(tempPath, radiPath);
+        filePath = files.data[0]["path"];
 
         var obj = {};
         var results = [];
-        fs.createReadStream(tempPath)
+        fs.createReadStream(filePath)
             .pipe(csv())
             .on('data', (data) => results.push(data))
             .on('end', () => {
-                obj["temp"] = results;
-                results = [];
-
-                fs.createReadStream(radiPath)
-                    .pipe(csv())
-                    .on("data", (data) => results.push(data))
-                    .on("end", () => {
-                        obj["radi"] = results;
-                        res.render("view", {
-                            data: obj
-                        });
-                    })
+                res.render("view", {
+                    data: results
+                });
         });
     });
 })
